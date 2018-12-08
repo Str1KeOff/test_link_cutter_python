73d0889d1c8cbfa53dcb986427b216ce31eb3cee
Str1KeOff <strikeoffical@mail.ru>
2018-12-08 19:18:28 +0300

    4 Nikita

--- blog.py
+++ blog.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', links=get_links())
+    return render_template('index.html', page_title="Main page", user={"name": "test"})
 
 
-@app.route('/link/<shorter_link>')
-def redirect_to_link(shorter_link):
-    return redirect(get_full_link(shorter_link))
-
-
-@app.route('/cut_link', methods=["POST"])
-def cut_link():
-    link = request.form['link']
-    link_name = request.form['link_name']
-    add_link(link_name, link)
-    return redirect('/')
+# @app.route('/link/<shorter_link>')
+# def redirect_to_link(shorter_link):
+#     return redirect(get_full_link(shorter_link))
+#
+#
+# @app.route('/cut_link', methods=["POST"])
+# def cut_link():
+#     link = request.form['link']
+#     link_name = request.form['link_name']
+#     add_link(link_name, link)
+#     return redirect('/')
 
 
 if __name__ == "__main__":
